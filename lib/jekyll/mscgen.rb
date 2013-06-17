30c5102e6e2892f97ca9b2849e9f67e7331e4e67
Wiktor Macura <wmacura@gmail.com>
2013-06-17 19:17:06 -0400

    ic

---
+++ lib/jekyll/mscgen.rb
@@ -0,0 +1,7 @@
+require "jekyll/mscgen/version"
+
+module Jekyll
+  module Mscgen
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/mscgen/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module Mscgen
+    VERSION = "0.0.1"
+  end
+end
